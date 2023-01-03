80b3b725b23568f600af1f8a65851ef03c5f4617
Ing Ondrej Hejda <ondrej.hejda@te.com>
2023-01-03 09:48:40 +0100

    detect figure closed and run forever

---
+++ testPlotClosed.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+""" 
+Interactive plot example by:
+    https://stackoverflow.com/questions/4098131/how-to-update-a-plot-in-matplotlib
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 6*np.pi, 100)
+y = np.sin(x)
+
+# You probably won't need this if you're embedding things in a tkinter plot...
+plt.ion()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
+
+phase = 0
+step = 10 * np.pi / 500
+
+while True:
+
+    phase += step
+    if phase > 2*np.pi:
+        phase -= 2*np.pi
+
+    print(phase)
+
+    if plt.fignum_exists(fig.number):
+        line1.set_ydata(np.sin(x + phase))
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+    else:
+        print('Plot closed!')
+        break
