339ee8faa52019be3d834e88f920e84a570f19a9
Ing Ondrej Hejda <ondrej.hejda@te.com>
2023-01-04 09:31:18 +0100

    timing update (same function)

--- gyrousb/gyrousb.ino
+++ gyrousb/gyrousb.ino
@@ -28,29 +28,32 @@ void setup(void) {
 }
 
 void loop() {
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  uint32_t now = millis();
 
-  //Serial.print(millis());
-  //Serial.print(" ");
+  if ((now - tlast) >= PERIOD_MS) {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
 
-  Serial.print(a.acceleration.x);
-  Serial.print(" ");
-  Serial.print(a.acceleration.y);
-  Serial.print(" ");
-  Serial.print(a.acceleration.z);
-  Serial.print(" ");
+    //Serial.print(millis());
+    //Serial.print(" ");
 
-  Serial.print(g.gyro.x);
-  Serial.print(" ");
-  Serial.print(g.gyro.y);
-  Serial.print(" ");
-  Serial.print(g.gyro.z);
-  Serial.println(" ");
+    Serial.print(a.acceleration.x);
+    Serial.print(" ");
+    Serial.print(a.acceleration.y);
+    Serial.print(" ");
+    Serial.print(a.acceleration.z);
+    Serial.print(" ");
 
-  //Serial.print(temp.temperature);
-  //Serial.println("");
+    Serial.print(g.gyro.x);
+    Serial.print(" ");
+    Serial.print(g.gyro.y);
+    Serial.print(" ");
+    Serial.print(g.gyro.z);
+    Serial.println("");
 
-  while ((millis() - tlast) < PERIOD_MS);
-  tlast += PERIOD_MS;
+    //Serial.print(temp.temperature);
+    //Serial.println("");
+
+    tlast += PERIOD_MS;
+  }
 }
