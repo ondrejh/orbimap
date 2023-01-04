8f0f9452b49c8dfa4c7cf5640a8a3e3999af2a33
Ing Ondrej Hejda <ondrej.hejda@te.com>
2023-01-04 11:55:23 +0100

    plot roll too

--- orbimap.py
+++ orbimap.py
@@ -13,17 +13,19 @@ import threading
 import serial
 from time import sleep
 
-TMAX = 5.0 #s
 LEN = 250
-LIM = [-12, 12]
+TMAX = LEN * 0.02 #s
+ALIM = [-12, 12]
+RLIM = [-10, 10]
 
 PORT = 'COM9'
 
 class myFig(object):
 
-    def __init__(self, fig, tmax=TMAX, ylim=LIM, dlen=LEN):
+    def __init__(self, fig, tmax=TMAX, ylim=ALIM, zlim=RLIM, dlen=LEN):
         self.fig = fig
-        self.ax = self.fig.add_subplot(111)
+        self.ax = self.fig.add_subplot(211)
+        self.bx = self.fig.add_subplot(212)
 
         self.phase = 0
         self.step = 0.1
@@ -31,30 +33,55 @@ class myFig(object):
         self.dlen = dlen
         self.tmax = tmax
         self.ylim = ylim
+        self.zlim = zlim
+
         self.x = np.linspace(0, self.tmax, self.dlen)
-        self.y1 = [0] * self.dlen #np.sin(self.x)
-        self.y2 = [0] * self.dlen #np.sin(self.x + 2 * np.pi / 3)
-        self.y3 = [0] * self.dlen #np.sin(self.x + np.pi / 3)
+        self.y1 = [0] * self.dlen
+        self.y2 = [0] * self.dlen
+        self.y3 = [0] * self.dlen
+        self.z1 = [0] * self.dlen
+        self.z2 = [0] * self.dlen
+        self.z3 = [0] * self.dlen
+
         self.ln1, self.ln2, self.ln3, = self.ax.plot(self.x, self.y1, 'r-', self.x, self.y2, 'g-', self.x, self.y3, 'b-')
         self.ax.set_ylim(self.ylim)
         self.ax.set_xlim([0, self.tmax])
         self.ax.grid()
+
+        self.rln1, self.rln2, self.rln3, = self.bx.plot(self.x, self.z1, 'r-', self.x, self.z2, 'g-', self.x, self.z3, 'b-')
+        self.bx.set_ylim(self.zlim)
+        self.bx.set_xlim([0, self.tmax])
+        self.bx.grid()
+
         self.newdata = False
 
-    def reset(self, tmax=TMAX, ylim=LIM, dlen=LEN):
+    def reset(self, tmax=TMAX, ylim=ALIM, zlim=RLIM, dlen=LEN):
         print('reset')
         self.ax.clear()
+
         self.dlen = dlen
         self.tmax = tmax
         self.ylim = ylim
+        self.zlim = zlim
+
         self.x = np.linspace(0, self.tmax, self.dlen)
-        self.y1 = [0] * self.dlen #np.sin(self.x)
-        self.y2 = [0] * self.dlen #np.sin(self.x + 2 * np.pi / 3)
-        self.y3 = [0] * self.dlen #np.sin(self.x + np.pi / 3)
+        self.y1 = [0] * self.dlen
+        self.y2 = [0] * self.dlen
+        self.y3 = [0] * self.dlen
+        self.z1 = [0] * self.dlen
+        self.z2 = [0] * self.dlen
+        self.z3 = [0] * self.dlen
+
         self.ln1, self.ln2, self.ln3, = self.ax.plot(self.x, self.y1, 'r-', self.x, self.y2, 'g-', self.x, self.y3, 'b-')
         self.ax.set_ylim(self.ylim)
         self.ax.set_xlim([0, self.tmax])
         self.ax.grid()
+
+        self.rln1, self.rln2, self.rln3, = self.bx.plot(self.x, self.z1, 'r-', self.x, self.z2, 'g-', self.x, self.z3, 'b-')
+        self.bx.set_ylim(self.zlim)
+        self.bx.set_xlim([0, self.tmax])
+        self.bx.grid()
+
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
@@ -64,22 +91,31 @@ class myFig(object):
             self.y1[n] = self.y1[n - 1]
             self.y2[n] = self.y2[n - 1]
             self.y3[n] = self.y3[n - 1]
+            self.z1[n] = self.z1[n - 1]
+            self.z2[n] = self.z2[n - 1]
+            self.z3[n] = self.z3[n - 1]
         self.y1[0] = data[0]
         self.y2[0] = data[1]
         self.y3[0] = data[2]
+        self.z1[0] = data[3]
+        self.z2[0] = data[4]
+        self.z3[0] = data[5]
         self.newdata = True
-        #self.update()
 
     def update(self):
         if self.newdata:
-            print('update')
+            #print('update')
             self.ln1.set_ydata(self.y1)
             self.ln2.set_ydata(self.y2)
             self.ln3.set_ydata(self.y3)
+            self.rln1.set_ydata(self.z1)
+            self.rln2.set_ydata(self.z2)
+            self.rln3.set_ydata(self.z3)
             self.fig.canvas.draw()
             self.fig.canvas.flush_events()
             self.newdata = False
-        root.after(100, self.update)
+        root.after(20, self.update)
+
 
 class GetGyro(threading.Thread):
     def __init__(self, port, update):
@@ -93,22 +129,19 @@ class GetGyro(threading.Thread):
             while not self.stop:
                 cont = p.read()
                 for b in cont:
-                    if b not in (ord('\n'), ord('\r')):
+                    if b != ord('\n'):
                         buf += chr(b)
                     else:
                         splt = buf.split(' ')
                         if len(splt) == 6:
                             val = [float(s) for s in splt]
-                            print(val)
+                            #print(val)
                             try:
                                 self.updatefcn(val)
                             except:
                                 print('finito')
                                 self.stop = True
                         buf = ""
-
-def update(data):
-    pass
 
 
 # frames
